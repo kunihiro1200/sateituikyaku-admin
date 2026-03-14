96a0faab6d06fe37a2610c18594bbe466b6f4a97
kunihiro1200 <tomoko.kunihiro@ifoo-oita.com>
2026-03-14 11:03:50 +0900

    feat: show land templates only when property_type is 土, otherwise show house/mansion template

--- frontend/frontend/src/pages/BuyerDetailPage.tsx
+++ frontend/frontend/src/pages/BuyerDetailPage.tsx
@@ -817,15 +817,19 @@ TEL：097-533-2022`;
                 }}
               >
                 <MenuItem value="">テンプレート選択</MenuItem>
-                <MenuItem value="land_no_permission">資料請求（土）許可不要</MenuItem>
-                <MenuItem value="minpaku">民泊問合せ</MenuItem>
-                <MenuItem value="land_need_permission">資料請求（土）売主要許可</MenuItem>
+                {/* 種別が土地の場合のみ土地用テンプレートを表示 */}
+                {linkedProperties[0]?.property_type === '土' ? [
+                  <MenuItem key="land_no_permission" value="land_no_permission">資料請求（土）許可不要</MenuItem>,
+                  <MenuItem key="minpaku" value="minpaku">民泊問合せ</MenuItem>,
+                  <MenuItem key="land_need_permission" value="land_need_permission">資料請求（土）売主要許可</MenuItem>,
+                ] : [
+                  <MenuItem key="house_mansion" value="house_mansion">資料請求（戸・マ）</MenuItem>,
+                ]}
                 <MenuItem value="offer_no_viewing">買付あり内覧NG</MenuItem>
                 <MenuItem value="offer_ok_viewing">買付あり内覧OK</MenuItem>
                 <MenuItem value="no_response">前回問合せ後反応なし</MenuItem>
                 <MenuItem value="no_response_offer">反応なし（買付あり不適合）</MenuItem>
                 <MenuItem value="pinrich">物件指定なし（Pinrich）</MenuItem>
-                <MenuItem value="house_mansion">資料請求（戸・マ）</MenuItem>
               </Select>
             </FormControl>
           )}
