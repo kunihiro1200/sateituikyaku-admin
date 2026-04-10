21fd227ecc4a47da0594efb12b7d217e89d19fc8
kunihiro1200 <tomoko.kunihiro@ifoo-oita.com>
2026-04-10 12:58:35 +0900

    feat: 通話モードページのコメントクイックボタンに「お客様います」「当社紹介」を追加

--- frontend/frontend/src/pages/CallModePage.tsx
+++ frontend/frontend/src/pages/CallModePage.tsx
@@ -6611,6 +6611,52 @@ HP：https://ifoo-oita.com/
                     }),
                   }}
                 />
+                <Chip
+                  label="お客様います"
+                  onClick={() => {
+                    handleQuickButtonClick('call-memo-has-customer');
+                    appendBoldText('お客様からこの辺で探していると問合せがあったときにご紹介は控えたほうが良いですよね？');
+                  }}
+                  size="small"
+                  clickable
+                  disabled={isButtonDisabled('call-memo-has-customer')}
+                  sx={{
+                    backgroundColor: '#fce4ec',
+                    ...(getButtonState('call-memo-has-customer') === 'pending' && {
+                      backgroundColor: '#fff9c4',
+                      textDecoration: 'line-through',
+                      color: 'text.secondary',
+                    }),
+                    ...(getButtonState('call-memo-has-customer') === 'persisted' && {
+                      backgroundColor: '#e0e0e0',
+                      textDecoration: 'line-through',
+                      color: 'text.disabled',
+                    }),
+                  }}
+                />
+                <Chip
+                  label="当社紹介"
+                  onClick={() => {
+                    handleQuickButtonClick('call-memo-our-referral');
+                    appendBoldText('当社紹介済み');
+                  }}
+                  size="small"
+                  clickable
+                  disabled={isButtonDisabled('call-memo-our-referral')}
+                  sx={{
+                    backgroundColor: '#fce4ec',
+                    ...(getButtonState('call-memo-our-referral') === 'pending' && {
+                      backgroundColor: '#fff9c4',
+                      textDecoration: 'line-through',
+                      color: 'text.secondary',
+                    }),
+                    ...(getButtonState('call-memo-our-referral') === 'persisted' && {
+                      backgroundColor: '#e0e0e0',
+                      textDecoration: 'line-through',
+                      color: 'text.disabled',
+                    }),
+                  }}
+                />
               </Box>
             </Box>
 
