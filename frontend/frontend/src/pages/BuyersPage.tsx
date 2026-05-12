0e562f70382a22fba57b37e1f65fcc554dc3da74
kunihiro1200 <tomoko.kunihiro@ifoo-oita.com>
2026-05-12 17:59:04 +0900

    feat: add broker inquiry list button to BuyersPage header

--- frontend/frontend/src/pages/BuyersPage.tsx
+++ frontend/frontend/src/pages/BuyersPage.tsx
@@ -615,6 +615,20 @@ export default function BuyersPage() {
             他社物件新着配信
           </Button>
           <Button
+            variant="outlined"
+            onClick={() => window.open('https://docs.google.com/spreadsheets/d/1Ot8NeypzS4pftxF7c3yE635QWDcAMXia_PK6uhbsrl0/edit?gid=1106587220#gid=1106587220', '_blank')}
+            sx={{
+              borderColor: '#43a047',
+              color: '#43a047',
+              '&:hover': {
+                borderColor: '#2e7d32',
+                backgroundColor: '#43a04715',
+              },
+            }}
+          >
+            業者問合一覧
+          </Button>
+          <Button
             variant="contained"
             onClick={() => navigate('/tateuri/manage')}
             sx={{
