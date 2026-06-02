8ea88a39b4cbaf138656e27c9c138f8e66e5646a
kunihiro1200 <tomoko.kunihiro@ifoo-oita.com>
2026-06-02 17:17:27 +0900

    debug: always show checkbox to confirm rendering works

--- frontend/frontend/src/components/PurchaseApplicationPrintSheet.tsx
+++ frontend/frontend/src/components/PurchaseApplicationPrintSheet.tsx
@@ -50,11 +50,9 @@ const PurchaseApplicationPrintSheet = React.forwardRef<HTMLDivElement, PurchaseA
         }}
       >
         {/* 10万円キャンペーンチェックボックス（1500万超のみ） */}
-        {propertyPrice && Number(propertyPrice) > 15000000 && (
-          <Box sx={{ position: 'absolute', top: '8mm', right: '14mm', fontSize: '16pt' }}>
-            ☐
-          </Box>
-        )}
+        <Box sx={{ position: 'absolute', top: '8mm', right: '14mm', fontSize: '16pt' }}>
+          ☐
+        </Box>
         {/* タイトル */}
         <Typography
           sx={{
